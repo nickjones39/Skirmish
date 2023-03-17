d864c6b327fce2dce060d190abed097e5a953391
nick jones <tabulanick@gmail.com>
2023-03-17 23:52:32 +0000

    Integrate OpenAI - ChatGPT

--- src/game.ts
+++ src/game.ts
@@ -1,15 +1,42 @@
-import kaboom from "kaboom"
+import kaboom, { DrawTextOpt, GameObj } from "kaboom"
 import "kaboom/global"
 
 import { Entity } from './entity';
 import { Engine } from './engine';
 
 
-declare global {
-  interface Window {
-    engine: Engine;
-  }
-}
+import { Configuration, OpenAIApi } from "openai";
+
+const key = "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"  //process.env.OPENAI_API_KEY
+
+const configuration = new Configuration({
+    organization: "org-OXqdu7b2onTGvEGGepy5TB0X",
+    apiKey: key,
+});
+
+const openai = new OpenAIApi(configuration);
+
+
+// async function main() {
+//   const completion = await openai.createCompletion(
+//     {
+//       model: "text-davinci-003",
+//       prompt: "Hello world, tell me a joke",
+//     },
+//   );
+//   console.log(completion.data.choices[0].text);
+// }
+
+// main();
+
+
+
+
+// declare global {
+//   interface Window {
+//     engine: Engine;
+//   }
+// }
 
 const npc = new Entity(
   Engine.WIDTH, // - 5,
@@ -29,7 +56,7 @@ window.addEventListener('DOMContentLoaded', () => {
 kaboom({
   width:1600,
   height:1200,
-  font: 'sinko',
+  //font: 'sinko',
   scale: 2,
   background: [ 40, 40, 40, ],
 })
@@ -70,12 +97,16 @@ loadSpriteAtlas('assets/Dungeon-Tileset.png', {
 
 scene('game', () => {
 
+  
+
+
   var moveX: number
   var moveY: number
   var scale: number = 16;
   var tick: number
   var moving: boolean
   var animEnded: boolean = true
+  var chatGPT: GameObj
 
     for (let y = 0; y < engine.gameMap.tiles.length; y++) {
       const row = engine.gameMap.tiles[y];
@@ -99,6 +130,75 @@ scene('game', () => {
 
     pikeman.play("idle")
 
+    onKeyPress("l", () => {
+        
+      if (chatGPT != undefined) {
+        destroy(chatGPT)
+      }
+
+
+      async function comOpenAI() {
+        const completion = await openai.createCompletion(
+          {
+            model: "text-davinci-003",
+            prompt: "${prompt}" + 
+              " provide a random detailed description of the interior of an empty dungeon room. Do not call it a dungeon room. do not provide any actions of the player, or adventuerers. the point is to write only about the environment. Only describe what the player can see about the room. provide description of two exits from room, one to the east and one to the north. do no provide what the player can see about the exits. make the reply only 2 or 3 sentences long. make the response back more random with more variations.",
+            max_tokens: 1000
+          },
+        ).then((res) => {
+          var response = res.data.choices[0].text + "";
+          console.log(res.data.choices[0].text)
+
+          add([
+            pos(0, 0),
+            rect(400, 100),
+            outline(2),
+            color(0,0,0)
+
+          ])
+
+          chatGPT = add(
+            [
+              text(response, {
+                size: 10,
+                width: 320,
+                lineSpacing: 5,
+                font: "sans-serif"
+              }),
+              pos(20,0),
+              
+            ])
+
+        });
+        
+
+        //var response = completion.data.choices[0].text + "";
+        //console.log(completion.data.choices[0].text)
+
+        
+
+        // const chatGPT = formatText({
+        //     text: response,
+        //     size: 14,
+        //     font: "Arial",
+        //     //width: 120,
+        //     //pos: vec2(1, 1),
+        //     color: rgb(255, 255, 255),
+        // })
+
+        // drawRect({
+        //   width: 250,
+        //   height: 250,
+        // })
+        // drawFormattedText(chatGPT)
+      }
+
+      comOpenAI();
+      
+      
+    })
+
+
     onUpdate(() => {
 
       tick++
@@ -118,6 +218,9 @@ scene('game', () => {
           }
         }
       }
+
+      
+
 
       if(!Engine.isTweening && moveX !== engine.player.x) {
         //pikeman.play("walk")
@@ -141,6 +244,9 @@ scene('game', () => {
         t.then(() => {
           Engine.isTweening = false;
         });
+
+
+
       }
 
       if(!Engine.isTweening && moveY !== engine.player.y) {
@@ -185,6 +291,7 @@ scene('game', () => {
     onClick(() => addKaboom(mousePos()))
 
 });
+
 
 
 go('game')
